d5c25feae2415b03a65db7b9757648eee17949bb
Andy Pickler <andy.pickler@gmail.com>
2023-06-03 13:23:50 -0600

    Solution to 1386 - doesn't scale well

--- 1386_cinema_seats/solution.ts
+++ 1386_cinema_seats/solution.ts
@@ -1,6 +1,55 @@
 export function maxNumberOfFamilies(
-  n: number,
+  rows: number,
   reservedSeats: number[][]
 ): number {
-  return 1;
+  const reservedMap = new Map<number, number[]>();
+  const reservableMap = new Map<number, number[]>();
+  for (
+    let reservedSeat = 0;
+    reservedSeat < reservedSeats.length;
+    reservedSeat++
+  ) {
+    const [row, seat] = reservedSeats[reservedSeat];
+    if (reservedMap.has(row)) {
+      reservedMap.set(row, [...(reservedMap.get(row) as number[]), seat]);
+    } else {
+      reservedMap.set(row, [seat]);
+    }
+  }
+
+  const validSeats = [2, 4, 6];
+  for (let row = 1; row <= rows; row++) {
+    for (let i = 0; i < validSeats.length; i++) {
+      const seat = validSeats[i];
+      if (reservedMap.has(row)) {
+        let reservedSeats = reservedMap.get(row) as number[];
+        let desiredSeats = [seat, seat + 1, seat + 2, seat + 3];
+        if (
+          !reservedSeats.some((reservedSeat) =>
+            desiredSeats.includes(reservedSeat)
+          )
+        ) {
+          if (reservableMap.has(row)) {
+            const reservableSeats = reservableMap.get(row) as number[];
+            if (!reservableSeats.includes(seat - 2)) {
+              reservableMap.set(row, [...reservableSeats, seat]);
+            }
+          } else {
+            reservableMap.set(row, [seat]);
+          }
+        }
+      } else {
+        if (reservableMap.has(row)) {
+          const reservableSeats = reservableMap.get(row) as number[];
+          if (!reservableSeats.includes(seat - 2)) {
+            reservableMap.set(row, [...reservableSeats, seat]);
+          }
+        } else {
+          reservableMap.set(row, [seat]);
+        }
+      }
+    }
+  }
+
+  return Array.from(reservableMap.values()).flat().length;
 }
--- 1386_cinema_seats/test.ts
+++ 1386_cinema_seats/test.ts
@@ -15,3 +15,26 @@ it("n = 3, reservedSeats = [[1,2],[1,3],[1,8],[2,6],[3,1],[3,10]] is 4", () => {
     4
   );
 });
+
+it("n = 2, reservedSeats = [[2,1],[1,8],[2,6]] is 2", () => {
+  assert.equal(
+    maxNumberOfFamilies(2, [
+      [2, 1],
+      [1, 8],
+      [2, 6],
+    ]),
+    2
+  );
+});
+
+it("n = 4, reservedSeats = [[4,3],[1,4],[4,6],[1,7]] is 4", () => {
+  assert.equal(
+    maxNumberOfFamilies(4, [
+      [1, 4],
+      [1, 7],
+      [4, 3],
+      [4, 6],
+    ]),
+    4
+  );
+});

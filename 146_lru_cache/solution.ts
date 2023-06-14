16db7402f1f06c24c3b6c6d0e81bae01b6e588ef
Andy Pickler <andy.pickler@gmail.com>
2023-06-14 15:28:17 -0600

    Solution to 146 LRU Cache using just Map

--- 146_lru_cache/solution.ts
+++ 146_lru_cache/solution.ts
@@ -1,58 +1,27 @@
-class Node {
-  next!: Node;
-  prev!: Node;
-
-  constructor(public key: number, public val: number) {}
-}
-
 export class LRUCache {
-  private nodes = new Map<number, Node>();
-  private head = new Node(-1, -1);
-  private tail = new Node(-1, -1);
+  private nodes = new Map<number, number>();
 
-  constructor(private capacity: number) {
-    this.head.next = this.tail;
-    this.tail.prev = this.head;
-  }
+  constructor(private capacity: number) {}
 
   get(key: number): number {
     if (!this.nodes.has(key)) {
       return -1;
     }
 
-    const node = this.nodes.get(key) as Node;
-    this.remove(node);
-    this.add(node);
-    return node.val;
+    const value = this.nodes.get(key) as number;
+    this.nodes.delete(key);
+    this.nodes.set(key, value);
+
+    return value;
   }
 
   put(key: number, value: number): void {
-    if (this.nodes.has(key)) {
-      const node = this.nodes.get(key) as Node;
-      this.remove(node);
-    }
-
-    const node = new Node(key, value);
-    this.nodes.set(key, node);
-    this.add(node);
+    this.nodes.delete(key);
+    this.nodes.set(key, value);
 
     if (this.nodes.size > this.capacity) {
-      const node = this.head.next;
-      this.remove(node);
-      this.nodes.delete(node.key);
+      const firstKey = this.nodes.keys().next().value;
+      this.nodes.delete(firstKey);
     }
-  }
-
-  private add(node: Node) {
-    const previousEnd = this.tail.prev;
-    previousEnd.next = node;
-    node.prev = previousEnd;
-    node.next = this.tail;
-    this.tail.prev = node;
-  }
-
-  private remove(node: Node) {
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
   }
 }
